18e569253f4b06b87b7c688b7a640f1bd0020427
Raul Barrios Siles <raulbarriossiles@gmail.com>
2019-09-25 21:21:26 +0200

    refactor(calculator): copy and change the code to do with eval

---
+++ commandPatter/calculatorV2.js
@@ -0,0 +1,21 @@
+const command = {
+  add: '+',
+  subtract: '-',
+  multiply: '*',
+  division: '/',
+  execute: function(action, { num1, num2 }) {
+    return eval(num1 + this[action] + num2);
+  }
+};
+
+const calculate = (action, numbers) => {
+  const result = command.execute(action, numbers);
+  console.log(
+    `In ${action} of numbers ${Object.values(numbers)} the result is ${result}`
+  );
+};
+
+calculate('add', { num1: 2, num2: 3 });
+calculate('subtract', { num1: 9, num2: 5 });
+calculate('multiply', { num1: 7, num2: 2 });
+calculate('division', { num1: 8, num2: 4 });
