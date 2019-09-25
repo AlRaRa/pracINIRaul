7b1eb68f0f6afaf3fd49d0d03a0ba64165e27401
Raul Barrios Siles <raulbarriossiles@gmail.com>
2019-09-25 20:16:18 +0200

    test(calculator): add tries to command

--- commandPatter/calculator.js
+++ commandPatter/calculator.js
@@ -22,3 +22,8 @@ const calculate = (action, numbers) => {
     `In ${action} of numbers ${Object.values(numbers)} the result is ${result}`
   );
 };
+
+calculate('add', { num1: 2, num2: 3 });
+calculate('subtract', { num1: 9, num2: 5 });
+calculate('multiply', { num1: 7, num2: 2 });
+calculate('division', { num1: 8, num2: 4 });
