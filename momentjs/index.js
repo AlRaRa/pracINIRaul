e84b88376f3760523e496200d2e1df7ccabd5cde
Raul Barrios Siles <raulbarriossiles@gmail.com>
2019-09-25 23:37:09 +0200

    feat(): add diffDates

--- momentjs/index.js
+++ momentjs/index.js
@@ -26,10 +26,17 @@ const employees = [
   }
 ];
 
-const diffDates = (date1, date2) => {};
+const diffDates = (date1, date2) => {
+  return date1.isSame(date2);
+};
 
 const sumDates = (date, unitTimes) => {};
 
 const setDateEmployee = name => {};
 
 const sumDiffDate = () => {};
+
+const a = moment('2010-10-20');
+const b = moment('2010-10-20');
+
+diffDates(a, b);
