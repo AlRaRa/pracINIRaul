ebc89f7d50eabff2011cc33578730da51ba37158
Raul Barrios Siles <raulbarriossiles@gmail.com>
2019-09-25 20:12:19 +0200

    feat(calculator): added the function multiplication

--- commandPatter/calculator.js
+++ commandPatter/calculator.js
@@ -2,7 +2,7 @@ const addition = (num1, num2) => num1 + num2;
 
 const subtraction = (num1, num2) => num1 - num2;
 
-const multiplication = () => {};
+const multiplication = (num1, num2) => num1 * num2;
 
 const division = () => {};
 
