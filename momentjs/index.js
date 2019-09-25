4da13f6a1125fd9086b38751f97572a2f82fcd47
Raul Barrios Siles <raulbarriossiles@gmail.com>
2019-09-25 23:14:52 +0200

    chore(): add the structure

---
+++ momentjs/index.js
@@ -0,0 +1,34 @@
+const DATE_FORMAT = 'DD/MM/YYYY';
+const DATE_FORMAT_COMPLETE = 'DD/MM/YYYY HH:mm:sss';
+
+const employees = [
+  {
+    name: 'pepe',
+    money: 1000,
+    timezone: 'Europe/calaluya',
+    date: '10/01/2017 -- 00:30:00',
+    update_Up: ''
+  },
+  {
+    name: 'Luis',
+    money: 1500,
+    timezone: 'Australia/Sydney',
+    date: '13/10/2017 -- 10:30:00',
+    update_Up: ''
+  },
+  {
+    name: 'Perico',
+    money: 1200,
+    timezone: 'Europe/Madrid',
+    date: '15/05/2017 -- 01:30:00',
+    update_Up: ''
+  }
+];
+
+const diffDates = (date1, date2) => {};
+
+const sumDates = (date, unitTimes) => {};
+
+const setDateEmployee = name => {};
+
+const sumDiffDate = () => {};
